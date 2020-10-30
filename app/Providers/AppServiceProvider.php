f06093e63e34bce60b40a4eea9ace1ef2b3dfdef
KeiKey <keiselami1@gmail.com>
2020-10-30 14:52:15 +0100

    Redirect to Control Panel after authentication

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repository\Classes\CategoryRepository;
+use App\Repository\Classes\ProductRepository;
 use App\Repository\Classes\UserRepository;
+use App\Repository\Contracts\CategoryRepositoryInterface;
+use App\Repository\Contracts\ProductRepositoryInterface;
 use App\Repository\Contracts\UserRepositoryInterface;
+use App\Services\CategoryService;
+use App\Services\ProductService;
 use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,10 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(UserRepositoryInterface::class,
-        UserRepository::class);
+        $this->registerRepositories();
 
-        $this->app->instance(UserService::class, new UserService(new UserRepository($this->app)));
     }
 
     /**
@@ -30,5 +34,27 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
+        $this->bootServices();
+    }
+
+    private function registerRepositories()
+    {
+        $this->app->bind(UserRepositoryInterface::class,
+            UserRepository::class);
+
+        $this->app->bind(CategoryRepositoryInterface::class,
+            CategoryRepository::class);
+
+        $this->app->bind(ProductRepositoryInterface::class,
+            ProductRepository::class);
+    }
+
+    private function bootServices()
+    {
+        $this->app->instance(UserService::class, new UserService(new UserRepository($this->app)));
+
+//        $this->app->instance(CategoryService::class, new UserService(new CategoryRepository($this->app)));
+//
+//        $this->app->instance(ProductService::class, new UserService(new ProductRepository($this->app)));
     }
 }
